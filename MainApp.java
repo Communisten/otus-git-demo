2449231c1393ae26caa279bd4aa7bc08f3de65c1
Communisten <medveder@inbox.ru>
2024-01-29 02:26:36 +0300

    рисование квадрата

--- MainApp.java
+++ MainApp.java
@@ -1,5 +1,9 @@
 public class MainApp {
     public static void main(String[] args) {
-       System.out.printLn("Hello World");
+        System.out.printLn("Hello World");
+        System.out.printLn("---");
+        System.out.printLn("| |");
+        System.out.printLn("---");
+
     }
 }
